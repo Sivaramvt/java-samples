77ae2d5f55bdd03e85053313c245d8aaf1bbda8c
Sivaramvt <Sivaramvt@gmail.com>
2016-03-20 17:26:09 +0530

    Experimenting with String split function.

---
+++ src/main/java/com/computepatterns/experiments/StringSplitSample.java
@@ -0,0 +1,10 @@
+package com.computepatterns.experiments;
+
+/**
+ * Experimenting the String.split API.
+ */
+public class StringSplitSample {
+    public static void main(String[] args) {
+        System.out.println("category-and-doc-type:insights-and-education/brochure".split(":")[1]);
+    }
+}
