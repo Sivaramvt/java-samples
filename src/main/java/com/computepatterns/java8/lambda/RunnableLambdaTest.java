9c4a79f36133bf2a15730056ad9cae8b55f41ec1
Sivaramvt <Sivaramvt@gmail.com>
2016-03-30 15:05:41 +0530

    Added sample for java 8 lambda.

---
+++ src/main/java/com/computepatterns/java8/lambda/RunnableLambdaTest.java
@@ -0,0 +1,23 @@
+package com.computepatterns.java8.lambda;
+
+/**
+ * This example uses lambda to create a new thread
+ */
+public class RunnableLambdaTest {
+    public static void main(String[] args) {
+
+        // Using the anonymous inner class.
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Thread created and run using anonymous inner class.");
+            }
+        };
+        new Thread(task).start();
+
+        // Using lambda
+        new Thread(() -> {
+            System.out.println("Thread created and run using the lambda construct.");
+        }).start();
+    }
+}
